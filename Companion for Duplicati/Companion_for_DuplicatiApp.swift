90382cc3c4baa9866ac69d1e8e0f7a4ae8109a27
GatzeStreicheln <login@gatze.ch>
2026-04-09 02:26:08 +0200

    Initial Commit

---
+++ Companion for Duplicati/Companion_for_DuplicatiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Companion_for_DuplicatiApp.swift
+//  Companion for Duplicati
+//
+//  Created by Vasco Kugler on 09.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Companion_for_DuplicatiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Companion for Duplicati/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Companion for Duplicati
+//
+//  Created by Vasco Kugler on 09.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
